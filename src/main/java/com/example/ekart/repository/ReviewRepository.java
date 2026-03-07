6b42fa0af99699bb3fc53af353cc796c803976cd
shimasanjay <sanjaysanjue97@gmail.com>
2026-03-07 15:05:12 +0530

    Remove folder from repository

--- src/main/java/com/example/ekart/repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ekart.repository;
-
-import com.example.ekart.dto.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Integer> {
-}
