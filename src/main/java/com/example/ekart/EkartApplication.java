a19f90bbd0b00c21884d9045ca43e259c3844458
shimasanjay <sanjaysanjue97@gmail.com>
2026-03-15 21:33:08 +0530

    Sanjay: customer home

--- src/main/java/com/example/ekart/EkartApplication.java
+++ src/main/java/com/example/ekart/EkartApplication.java
@@ -2,8 +2,10 @@ package com.example.ekart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class EkartApplication {
 
 	public static void main(String[] args) {
