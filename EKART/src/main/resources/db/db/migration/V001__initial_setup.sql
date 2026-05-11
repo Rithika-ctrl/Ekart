fa33cf0673ebafd89f13bf5c3e67bd908836b078
B Venkatesh <2004venkateshb@gmail.com>
2026-05-11 23:45:08 +0530

    Empty DB's

---
+++ EKART/src/main/resources/db/db/migration/V001__initial_setup.sql
@@ -0,0 +1,6 @@
+-- V001: Initial Schema Setup
+-- This migration represents the baseline schema that was already present in the database
+-- The actual schema creation is handled by Hibernate JPA
+-- This placeholder allows Flyway to track the migration history
+
+-- Note: No DDL changes here as schema is created by JPA/Hibernate
